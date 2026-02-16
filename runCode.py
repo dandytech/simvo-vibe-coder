c66d386bd6e4ae54018b7c90e2cb8a3ca6f29a03
amaechinwankwo <damaechi@rocketmail.com>
2026-02-16 15:53:34 +0100

    Added runCode file

---
+++ runCode.py
@@ -0,0 +1,11 @@
+print("Hello, Welcome to the Adder!")
+
+def add(a, b):
+    return a+b
+
+number1 = 10
+number2 = 20
+
+print("Adding numbers" + str(number1) + " and "+ str(number2))
+sum = add(number1, number2)
+print("The sum is: "+ str(sum))
