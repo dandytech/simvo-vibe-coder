609f47394b76c3f5f3d0a1cb3b868acdd712e9ad
amaechinwankwo <damaechi@rocketmail.com>
2026-02-18 01:58:17 +0100

    Added JS logic to website

--- server.py
+++ server.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI, Query
 from request import search_species  # import your function
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+from pathlib import Path
 
 app = FastAPI()
+
+BASE_DIR = Path(__file__).resolve().parent
 
 @app.get("/hello")
 def say_hello():
@@ -11,3 +16,8 @@ def say_hello():
 def get_species(search: str = Query(...)):
     results = search_species(search)
     return {"data": results}
+
+@app.get("/", response_class=FileResponse)
+def return_site():
+    # Serve the HTL file directly
+    return FileResponse(BASE_DIR / "templates" / "index.html", media_type="text/html")
